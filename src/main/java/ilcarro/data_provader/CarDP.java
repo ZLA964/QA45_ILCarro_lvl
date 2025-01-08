f779f9e58e5ef96ace6abf87984b3c228cfd3811
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2025-01-08 15:32:33 +0200

    login.properties

--- src/main/java/ilcarro/data_provader/CarDP.java
+++ src/main/java/ilcarro/data_provader/CarDP.java
@@ -4,7 +4,6 @@ import ilcarro.dto.CarDto;
 import org.testng.annotations.DataProvider;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
